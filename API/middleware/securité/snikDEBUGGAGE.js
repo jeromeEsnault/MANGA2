b45ea35f7ad3e3cf5341df4c208ff7e86aefe975
esnault <jeromedev@esnault.link>
2020-11-27 21:58:22 +0100

    add snyk app

---
+++ API/middleware/securité/snikDEBUGGAGE.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+request({
+  method: 'POST',
+  url: 'https://private-anon-f05c1013db-snyk.apiary-proxy.com/api/v1/org/0f93fd49-edd9-48ce-9e11-294da69b12c3/integrations/475fac73-0937-4241-b07f-3202e5cfb8ea/import',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Authorization': 'token API_KEY'
+  },
+  body: "{  \"target\": {    \"owner\": \"\",    \"name\": \"\",    \"branch\": \"\"  },  \"files\": [    {      \"path\": \"\"    }  ]}"
+}, function (error, response, body) {
+  console.log('Status:', response.statusCode);
+  console.log('Headers:', JSON.stringify(response.headers));
+  console.log('Response:', body);
+});
