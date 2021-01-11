cd0a757d39b6bb09a8589b0860c90da85e04c078
esnault <jeromedev@esnault.link>
2021-01-11 23:52:35 +0100

    add 11/01/2021

--- public/js/filterSearchadmin.js
+++ public/js/filterSearchadmin.js
@@ -11,12 +11,34 @@ function myFunctionAdmin() {
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < filterManga.length; i++) {
-        a = filterManga[i].getElementsByTagName("li")[0];
+        a = filterManga[i].getElementById('titlevf')[0];
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             filterManga[i].style.display = "";
         } else {
             filterManga[i].style.display = "none";
+        }
+    }
+}
+
+function myFunctionAdminVolume() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
     }
 }
